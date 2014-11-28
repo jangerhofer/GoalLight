ed4cc80ec6ae2fcd7fe8b245a36addc4c73965c2
jangerhofer <jd.angerhofer@gmail.com>
2014-11-28 02:10:55 -0600

    General Cleaning
    Default "goal horn" added for teams that are not currently present in goalHorns/ directory.  No longer differentiate between home and away goals.
    
    Signed-off-by: jangerhofer <jd.angerhofer@gmail.com>

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 var request = require('request');
 var Player = require('player');
 var dateString =  new Date().getFullYear() + "-" + (new Date().getMonth() + 1) + "-" + (new Date().getDate())
-var queryString = 'http://live.nhle.com/GameData/GCScoreboard/' + dateString + '.jsonp'
+var queryString = "http://localhost:8888/test.jsonp"
+//'http://live.nhle.com/GameData/GCScoreboard/' + dateString + '.jsonp'
 
 var scoreDB = {};
 console.log(queryString);
 //  Deal with missing music files.
 process.on('uncaughtException', function (err) {
-  console.log(err);
+  var player = new Player('goalHorns/Default.mp3');
+  player.stop();
+  player.play();
 })
 
 
@@ -35,14 +38,9 @@ function updateScores(callback) {
     for (i = 0; i < results.length; i++) {
         if (results[i].ats != scoreDB[results[i].ata] && results[i].ats != 0) {
           console.log(new Date() + ".  Away Goal scored by: " + results[i].ata);
-          try {
-            var player = new Player('goalHorns/' + results[i].ata + '.mp3');
-            player.stop();
-            player.play();
-          }
-          catch(err) {
-            console.log(results[i].ata + " goal horn not found.");
-          }
+          var player = new Player('goalHorns/' + results[i].ata + '.mp3');
+          player.stop();
+          player.play();
 
           scoreDB[results[i].ata] = results[i].ats;
         }
@@ -50,15 +48,9 @@ function updateScores(callback) {
           console.log(new Date() + ".  Home Goal scored by: " + results[i].hta);
 
 
-          try {
-            var player = new Player('goalHorns/' + results[i].hta + '.mp3');
-            player.stop();
-            player.play();
-          }
-          catch(err) {
-            console.log(results[i].hta + " goal horn not found.");
-          }
-
+          var player = new Player('goalHorns/' + results[i].hta + '.mp3');
+          player.stop();
+          player.play();
 
           player.on('error', function(err){
             // when error occurs
